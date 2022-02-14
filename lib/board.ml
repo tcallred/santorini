9858bae907a95572fb91df8c1d11507e0eed95cf
Taylor Allred <taylor.allred21@gmail.com>
2022-02-14 10:22:11 -0700

    additions and fixes for board.ml

--- lib/board.ml
+++ lib/board.ml
@@ -10,7 +10,7 @@ let token_can_move_to_space tok space { spaces; players; _ } =
   let player1, player2 = players in
   let p1t1, p1t2 = player1 in
   let p2t1, p2t2 = player2 in
-  let other_toks = List.filter (fun t -> t = tok) [ p1t1; p1t2; p2t1; p2t2 ] in
+  let other_toks = List.filter (fun t -> t <> tok) [ p1t1; p1t2; p2t1; p2t2 ] in
   let current_level = level_at tok spaces in
   let next_level = level_at space spaces in
   let space_occupied = List.mem space other_toks in
@@ -29,7 +29,7 @@ let token_can_build_on_space tok space { spaces; players; _ } =
   let player1, player2 = players in
   let p1t1, p1t2 = player1 in
   let p2t1, p2t2 = player2 in
-  let all_toks = List.filter (fun t -> t = tok) [ p1t1; p1t2; p2t1; p2t2 ] in
+  let all_toks = List.filter (fun t -> t <> tok) [ p1t1; p1t2; p2t1; p2t2 ] in
   let space_occupied = List.mem space all_toks in
   let in_adjacent = List.mem space (adjacent_spaces tok) in
   let space_level = level_at space spaces in
@@ -37,3 +37,33 @@ let token_can_build_on_space tok space { spaces; players; _ } =
 
 let build_on_space space board =
   { board with spaces = build_on space board.spaces }
+
+let complete_turn board =
+  { board with turn = board.turn + 1; players = swap_players board.players }
+
+let spaces_tok_can_move_to tok board : space list =
+  List.filter
+    (fun s -> token_can_move_to_space tok s board)
+    (adjacent_spaces tok)
+
+let spaces_tok_can_build_on tok board : space list =
+  List.filter
+    (fun s -> token_can_build_on_space tok s board)
+    (adjacent_spaces tok)
+
+let possible_moves_for_tok tok board =
+  spaces_tok_can_move_to tok board
+  |> List.map (fun s ->
+         ( s,
+           spaces_tok_can_build_on s
+             (move_token_to_space tok s board |> Result.get_ok) ))
+
+let play_full_turn tok to_space build_space board =
+  board
+  |> move_token_to_space tok to_space
+  |> Result.get_ok |> build_on_space build_space |> complete_turn
+
+let tokens_height tok { spaces; _ } = SpacesMap.find tok spaces
+
+let heights_around_token tok { spaces; _ } =
+  List.map (fun s -> SpacesMap.find s spaces) (adjacent_spaces tok)
