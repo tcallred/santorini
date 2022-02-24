24644d62e4a07b8450593ca3859b8b21b647d0d4
Taylor Allred <taylor.allred21@gmail.com>
2022-02-24 16:28:43 -0700

    refactored the rep of moves

--- lib/board.ml
+++ lib/board.ml
@@ -2,6 +2,7 @@ open Spaces
 open Player
 
 type board = { turn : int; spaces : spaces; players : player * player }
+type move = Move of { from : space; dest : space } | Build of space
 
 let new_board players = { turn = 0; spaces = new_spaces; players }
 
@@ -36,8 +37,8 @@ let token_can_build_on_space tok space board =
   let space_occupied = List.mem space all_toks in
   let in_adjacent = List.mem space (adjacent_spaces tok) in
   let space_level = level_at space board.spaces in
-  let tok_on_win_space = tokens_height tok board = 3 in 
-  in_adjacent && (not space_occupied) && space_level < 4 && (not tok_on_win_space)
+  let tok_on_win_space = tokens_height tok board = 3 in
+  in_adjacent && (not space_occupied) && space_level < 4 && not tok_on_win_space
 
 let build_on_space space board =
   { board with spaces = build_on space board.spaces }
@@ -55,16 +56,33 @@ let spaces_tok_can_build_on tok board : space list =
     (fun s -> token_can_build_on_space tok s board)
     (adjacent_spaces tok)
 
-let possible_moves_for_tok tok board =
-  spaces_tok_can_move_to tok board
-  |> List.map (fun s ->
-         (tok, (s, spaces_tok_can_build_on s (move_token_to_space tok s board))))
+let play_move (m : move) board =
+  match m with
+  | Move { from; dest } -> move_token_to_space from dest board
+  | Build s -> build_on_space s board
 
+let possible_move_seqs_for_tok tok board : move list list =
+  let move_moves =
+    List.map
+      (fun s -> Move { from = tok; dest = s })
+      (spaces_tok_can_move_to tok board)
+  in
+  let move_seqs =
+    List.map
+      (fun m ->
+        let board_after_move = play_move m board in
+        let spot_after_move =
+          match m with Move { dest; _ } -> dest | Build _ -> raise Bad_token
+        in
+        List.map
+          (fun b -> [ m; Build b ])
+          (spaces_tok_can_build_on spot_after_move board_after_move))
+      move_moves
+  in
+  List.concat move_seqs
 
-let play_full_turn tok to_space build_space board =
-  let after_move = move_token_to_space tok to_space board in
-  if tokens_height to_space board = 3 then complete_turn after_move
-  else build_on_space build_space after_move |> complete_turn
+let play_full_turn (moves : move list) board =
+  List.fold_left (fun b m -> play_move m b) board moves |> complete_turn
 
 let heights_around_token tok board =
   List.map
--- lib/solver.ml
+++ lib/solver.ml
@@ -37,53 +37,17 @@ let rec minimax (board : Board.board) maximizing_player alpha beta depth :
   else
     let (t1, t2), _ = board.players in
     let moves =
-      possible_moves_for_tok t1 board @ possible_moves_for_tok t2 board
+      possible_move_seqs_for_tok t1 board @ possible_move_seqs_for_tok t2 board
     in
     let best = (none, if maximizing_player then Int.min_int else Int.max_int) in
     best_move moves board best alpha beta maximizing_player depth
 
-and best_move moves board best alpha beta maximizing_player depth =
-  match moves with
+and best_move move_seqs board best alpha beta maximizing_player depth =
+  match move_seqs with
   | [] -> best
-  | move :: rest_moves ->
+  | move_seq :: rest_moves ->
       let _, curr_best_score = best in
-      let tok, (to_space, build_moves) = move in
-      let next_board, score =
-        if tokens_height to_space board = 3 then
-          let next = play_full_turn tok to_space (1, 1) board in
-          minimax next (not maximizing_player) alpha beta (depth + 1)
-        else
-          best_build tok to_space build_moves board best alpha beta
-            maximizing_player depth
-      in
-      if maximizing_player then
-        let next_best =
-          if score > curr_best_score then (next_board, score) else best
-        in
-        let _, next_best_score = next_best in
-        let next_alpha = max alpha next_best_score in
-        if beta <= alpha then next_best
-        else
-          best_move rest_moves board next_best next_alpha beta maximizing_player
-            depth
-      else
-        let next_best =
-          if score < curr_best_score then (next_board, score) else best
-        in
-        let _, next_best_score = next_best in
-        let next_beta = max beta next_best_score in
-        if beta <= alpha then next_best
-        else
-          best_move rest_moves board next_best alpha next_beta maximizing_player
-            depth
-
-and best_build tok to_space build_moves board best alpha beta maximizing_player
-    depth =
-  match build_moves with
-  | [] -> best
-  | build :: rest_builds ->
-      let _, curr_best_score = best in
-      let next_board = play_full_turn tok to_space build board in
+      let next_board = play_full_turn move_seq board in
       let _, score =
         minimax next_board (not maximizing_player) alpha beta (depth + 1)
       in
@@ -95,8 +59,8 @@ and best_build tok to_space build_moves board best alpha beta maximizing_player
         let next_alpha = max alpha next_best_score in
         if beta <= alpha then next_best
         else
-          best_build tok to_space rest_builds board next_best next_alpha beta
-            maximizing_player depth
+          best_move rest_moves board next_best next_alpha beta maximizing_player
+            depth
       else
         let next_best =
           if score < curr_best_score then (some next_board, score) else best
@@ -105,8 +69,8 @@ and best_build tok to_space build_moves board best alpha beta maximizing_player
         let next_beta = max beta next_best_score in
         if beta <= alpha then next_best
         else
-          best_build tok to_space rest_builds board next_best alpha next_beta
-            maximizing_player depth
+          best_move rest_moves board next_best alpha next_beta maximizing_player
+            depth
 
 let board_after_chosen_move (board : Board.board) : Board.board =
   let b, _ = minimax board true Int.min_int Int.max_int 0 in
